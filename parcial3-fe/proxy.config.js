4cf5bdfa89e54222933fb88cf3de549b58c49d7a
Alba Sánchez Ibáñez <91136148+brisanx@users.noreply.github.com>
2023-12-15 05:10:40 +0100

    Delete parcial3-fe/proxy.config.js

--- parcial3-fe/proxy.config.js
+++
@@ -1,9 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use('/api', proxy({
-    target: 'https://localhost:8000',  
-    changeOrigin: true,
-    secure: false,
-  }));
-};
